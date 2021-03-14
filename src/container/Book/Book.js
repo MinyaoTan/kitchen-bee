eb383906716a88f8ae8c88e3f64428b86748fc01
minyaotan <mtan3@uw.edu>
2021-03-14 11:59:30 -0700

    add Recipe component

--- src/container/Book/Book.js
+++ src/container/Book/Book.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import RecipeList from '../RecipeList/RecipeList';
+
 class Book extends Component {
     render() {
         return (
             <div>
                 <p>Search</p>
-                
+                <RecipeList />
             </div>
         );
     }
--- src/container/RecipeList/RecipeList.js
+++ src/container/RecipeList/RecipeList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Recipe from '../../component/Recipe/Recipe';
+
 class RecipeList extends Component {
     render() {
         return (
             <div>
-                
+                <Recipe />
             </div>
         );
     }
