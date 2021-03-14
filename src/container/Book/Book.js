df1ae8fa466922e98ff60371f75fba69cea90e8f
minyaotan <mtan3@uw.edu>
2021-03-14 11:42:00 -0700

    first commit

---
+++ src/container/Book/Book.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+    render() {
+        return (
+            <div>
+                <p>Search</p>
+                
+            </div>
+        );
+    }
+}
+
+export default Book;
