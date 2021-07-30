9fcca4549658d63828ffdc944362fa7a6f9d2856
minyaotan <mtan3@uw.edu>
2021-07-30 12:50:11 -0700

    update README

--- src/container/Book/Book.js
+++ src/container/Book/Book.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import RecipeList from '../RecipeList/RecipeList';
+// import RecipeList from '../RecipeList/RecipeList';
 import Toolbar from '../../component/UI/Toolbar/Toolbar';
-import NewRecipe from '../../component/NewRecipe/NewRecipe';
-import Recipe from '../../component/Recipe/Recipe';
-import SearchResult from '../SearchResult/SearchResult';
+// import NewRecipe from '../../component/NewRecipe/NewRecipe';
+// import Recipe from '../../component/Recipe/Recipe';
+// import SearchResult from '../SearchResult/SearchResult';
+
+const SearchResult = React.lazy(() => import('../SearchResult/SearchResult'));
+const NewRecipe = React.lazy(() => import('../../component/NewRecipe/NewRecipe'));
+const Recipe = React.lazy(() => import('../../component/Recipe/Recipe'));
+const RecipeList = React.lazy(() => import('../RecipeList/RecipeList'));
+
 
 class Book extends Component {
     render() {
         return (
             <div>
-                <Toolbar />
-                <Switch>
-                    <Route path="/searchResult" component={SearchResult} />
-                    <Route path="/editRecipe:id" component={NewRecipe} />
-                    <Route path="/recipe:id" component={Recipe} />
-                    <Route path="/newRecipe" component={NewRecipe} />
-                    <Route path="/" component={RecipeList} />
-                </Switch>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Toolbar />
+                    <Switch>
+                        <Route path="/searchResult" component={SearchResult} />
+                        <Route path="/editRecipe:id" component={NewRecipe} />
+                        <Route path="/recipe:id" component={Recipe} />
+                        <Route path="/newRecipe" component={NewRecipe} />
+                        <Route path="/" component={RecipeList} />
+                    </Switch>
+                </Suspense>
             </div>
         );
     }
